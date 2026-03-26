424f6239776b4a9b3bbfbfae80e599cb7d9b36c3
Md. Anisur Rahman <poetanis@gmail.com>
2026-03-26 17:38:49 +0600

    Update flowchart.js

--- flowchart.js
+++ flowchart.js
@@ -188,30 +188,6 @@ function buildFlow(ast) {
         return join;
       }
 
-  case "LogicalExpression":
-  let op = node.operator;
-  if(op === "&&") op = "এবং";
-  if(op === "||") op = "অথবা";
-  return `${getTextBN(node.left)} ${op} ${getTextBN(node.right)}`;
-  
-  
-  case "BinaryExpression":
-  return `${getTextBN(node.left)} ${node.operator} ${getTextBN(node.right)}`;
-
-  case "LogicalExpression":
-  let op = node.operator;
-  if(op === "&&") op = "এবং";
-  if(op === "||") op = "অথবা";
-  return `${getTextBN(node.left)} ${op} ${getTextBN(node.right)}`;
-  
-
-  case "UnaryExpression": 
-  if(node.operator === "!"){
-    return `!(${getTextBN(node.argument)})`;
-  }
-  return `${node.operator}${getTextBN(node.argument)}`;
-  
-
        case "WhileStatement": {
         const wId = newId("while");
         nodes.push(`${wId}=>condition: যতক্ষণ (${getTextBN(node.test)})`);
