dfaa824404d9984f45c540f211e5e948300da47b
Md. Anisur Rahman <poetanis@gmail.com>
2026-03-29 12:31:22 +0600

    Update flowchart.js

--- flowchart.js
+++ flowchart.js
@@ -74,7 +74,6 @@ function banglaToJS(code){
     .replace(/অংশ/g,"slice")
     .replace(/বড়হাতেরঅক্ষর/g,"toUpperCase")
     .replace(/ছোটহাতেরঅক্ষর/g,"toLowerCase")
-    .replace (/খোঁজা/g, "find")
     .replace (/উপস্ট্রিং/g, "substr");
      
 }
